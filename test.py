08951543fe06db52860c2175aca638c8be80a643
mamamamad <mamadhub8@gmail.com>
2024-12-21 23:51:03 +0330

    #

--- test.py
+++ test.py
@@ -1,10 +1,16 @@
 import strore as st
 import json
-s = st.Store()
-s.curently_path_store = '/Users/mohamad/Documents/vscode/matlab/a.json'
-s.add_object_store('h',12,12)
+# s = st.Store()/
+
+a = {1:'2'}
+b={2:'3'}
+
+# with open('/Users/mohamad/Documents/vscode/matlab/a.json','a') as file:
+#     json.dump(a,file,indent=4)
+# with open('/Users/mohamad/Documents/vscode/matlab/a.json','a') as file:
+#     json.dump(b,file,indent=4)
 
 
 with open("/Users/mohamad/Documents/vscode/matlab/jsons_file/store/hasan.json",'r')as file:
-    s = json.load(file)
+    s = json.loads(file)
     print(s)
