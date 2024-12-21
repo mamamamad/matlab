c61e0027c044cddb7e36677f9a05475eb0b6d9f3
mamamamad <mamadhub8@gmail.com>
2024-12-21 20:56:44 +0330

    update menu

--- Menu.py
+++ Menu.py
@@ -3,34 +3,21 @@ import strore as st
 
 store = st.Store()
 print("Welcome to store")
-inp = int(input("1 = Customers \n2 = The seller\n3 = Admin\n4 = Exit\nPlease chose a option: "))
 def menu(inp):
 
 
           
         if inp == 1:
-            store.show_stores()
-            inp = input("1 = Show product\n2 = Shopping cart\n3 = Menu\n4 = Exit :")
-            if inp == '1':
-                store.show_store_product()
-            elif inp == '2':
-                store.shopping_cart()
-            elif inp == '3':
-                menu(1)
-            elif inp == '4':
-                exit()
-            else:
-                print("Bad input.Menu......")
-                menu()
+            store.menu()
         elif inp == 2 :
-            store.authorization_seller()
-            # self.show_product()
             pass
         elif inp == 3:
-            # store.create_store('hasan')
+            store.menu_admin()
             pass
             
         elif inp == 4:
-            print(" Thanks for coming.\nGood by")
+            print("Thanks for coming.\nGood by")
             exit(0) 
-menu(inp)
+while (1):
+    inp = int(input("1 = Customers \n2 = The seller\n3 = Admin\n4 = Exit\nPlease chose a option: "))
+    menu(inp)
--- test.py
+++ test.py
@@ -1,24 +1,10 @@
-# st = {"1":[1,2],"2":[3,4],"3":[5,6]}
-
-# for i , j in st.items():
-#     print(i,j)
-
-# import shamsi_datetime as s
-# l = s.ShamsiDateTime()
-# st[5]= l
-# h = str(st[5])
-# print(h)
-# print(type(h))
+import strore as st
+import json
+s = st.Store()
+s.curently_path_store = '/Users/mohamad/Documents/vscode/matlab/a.json'
+s.add_object_store('h',12,12)
 
 
-
-# from datetime import datetime
-
-# now = datetime.now()
-
-# current_time = str(now.strftime("%H:%M:%S"))
-# print(type(current_time))
-# print("Current Time =", current_time)
-
-
-file = open('fd.json','r') 
+with open("/Users/mohamad/Documents/vscode/matlab/jsons_file/store/hasan.json",'r')as file:
+    s = json.load(file)
+    print(s)
